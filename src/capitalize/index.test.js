dcff4135c009d9c27999c1d26da6961ac87985f5
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-04-06 11:40:08 +0100

    wrote 2 unit tests

---
+++ src/capitalize/index.test.js
@@ -0,0 +1,14 @@
+import capitalize from './index'
+
+describe('capitalizeWord', () => {
+    const word = "mock-word" 
+    let returnValue;
+
+    beforeEach(() => {
+        returnValue = capitalize(word)
+    });
+
+    it('should capitalize the first letter of the word', () => {
+        expect(returnValue).toEqual("Mock-word")
+    });
+})
--- src/clearAbilities/index.js
+++ src/clearAbilities/index.js
@@ -2,6 +2,7 @@ const clearAbilities = (abilities) => {
 	if (abilities.children.length > 0) {
 		abilities.innerHTML = '';
 	}
+	return abilities
 };
 
 export default clearAbilities;
